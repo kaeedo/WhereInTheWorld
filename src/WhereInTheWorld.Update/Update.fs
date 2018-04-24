485673d1e3542ce434c942893fc2fa75b53edf6c
Ito, Kai <Kai.Ito@zuehlke.com>
2018-04-24 14:58:07 +0200

    Add only needed countries

--- src/WhereInTheWorld.Update/Update.fs
+++ src/WhereInTheWorld.Update/Update.fs
@@ -5,31 +5,53 @@ open System.Data.SQLite
 open Dapper
 
 [<CLIMutable>]
-type Test =
+type Country =
     { Id: int
-      Name: string }
+      Code: string
+      Name: string
+      LocalizedName: string }
 
 
 module Update =
-    let private databaseFilename = ".\world.db"
+    let private databaseFilename = "./world.db"
     let private connectionString = sprintf "Data Source=%s;Version=3" databaseFilename
     let private connection = new SQLiteConnection(connectionString)
 
     connection.Open()
 
     let supportedCountries =
-        [ "CA", ("Canada", "Canada")
-          "DE", ("Germany", "Deutschland")
-          "US", ("United States of America", "United States of America") ]
-        |> dict
+        [ "CA", "Canada", "Canada"
+          "DE", "Germany", "Deutschland"
+          "US", "United States of America", "United States of America" ]
 
-    let addCountries =
+    let getAllCountries () =
         let sql =
             supportedCountries
-            |> Seq.map (fun kvp ->
-                let name, localizedName = kvp.Value
-                sprintf "('%s', '%s', '%s')" kvp.Key name localizedName
+            |> Seq.map (fun (code, _, _) ->
+                sprintf "'%s'" code
+            )
+            |> String.concat ","
+
+        connection.Query<Country>(sprintf "SELECT * FROM Country WHERE Code IN (%s)" sql)
+
+    let addCountries () =
+        let allCountries = getAllCountries ()
+        let sql =
+            supportedCountries
+            |> Seq.filter (fun (code, _, _) ->
+                allCountries
+                |> Seq.exists (fun ac ->
+                    ac.Code = code
+                )
+                |> not
+            )
+            |> Seq.map (fun countryData ->
+                let code, name, localizedName = countryData
+                sprintf "('%s', '%s', '%s')" code name localizedName
                 )
             |> String.concat ","
 
-        connection.Execute("INSERT INTO 'Country' ('Code', 'Name', 'LocalizedName') VALUES " + sql) |> ignore
+        if String.IsNullOrWhiteSpace(sql)
+        then ()
+        else
+            connection.Execute("INSERT INTO 'Country' ('Code', 'Name', 'LocalizedName') VALUES " + sql) |> ignore
--- src/WhereInTheWorld/Program.fs
+++ src/WhereInTheWorld/Program.fs
@@ -4,5 +4,5 @@ open WhereInTheWorld.Update
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
-    printfn "%O" <| Update.addCountries
+    printfn "%A" <| Update.getAllCountries()
     0 // return an integer exit code
