c09b3a23b65a7e2c660d0f4685c1ddd9e88c08c3
Kai Ito <kai.ito@gmail.com>
2018-05-27 23:06:39 +0200

    Save per country

--- src/WhereInTheWorld.Update/UpdateProcess.fs
+++ src/WhereInTheWorld.Update/UpdateProcess.fs
@@ -12,45 +12,9 @@ module UpdateProcess =
             code = countryCode
         )
 
-    let updateAll postalCodes =
-        try
-            //let stopWatch = System.Diagnostics.Stopwatch.StartNew()
-            printfn "%i" (postalCodes |> Seq.length)
-            //let countryCode, countryName, countryLocalizedName = getCountryInformation countryCode
-
-            do postalCodes
-                |> Seq.map (fun i ->
-                    { Id = Unchecked.defaultof<int>
-                      CountryCode = (getCountryInformation i.CountryCode).Item1
-                      CountryName = (getCountryInformation i.CountryCode).Item2
-                      CountryLocalizedName = (getCountryInformation i.CountryCode).Item3
-                      PostalCode = i.PostalCode
-                      PlaceName = i.PlaceName
-                      SubdivisionCode = i.SubdivisionCode
-                      SubdivisionName = i.SubdivisionName
-                      CountyName = i.CountyName
-                      CountyCode = i.CountyCode
-                      CommunityName = i.CommunityName
-                      CommunityCode = i.CommunityCode
-                      Latitude = i.Latitude
-                      Longitude = i.Longitude
-                      Accuracy = i.Accuracy }
-                )
-                |> DataAccess.insertPostalCodes
-                |> run
-
-            //stopWatch.Stop()
-
-            Result.Ok "Done"
-        with
-        | e -> Result.Error ("", e)
-
-
-    (* let updateCountry countryCode =
+    let updateCountry countryCode =
         job {
-            let! importedPostalCodes =
-                (DataDownload.downloadPostalCodesForCountry
-                >=> DataImport.readPostalCodesFile) countryCode
+            let! importedPostalCodes = DataImport.readPostalCodesFile countryCode
 
             match importedPostalCodes with
             | Error e -> return Result.Error (countryCode, e)
@@ -85,4 +49,4 @@ module UpdateProcess =
                     return Result.Ok countryCode
                 with
                 | e -> return Result.Error (countryCode, e)
-        } *)
+        }
--- src/WhereInTheWorld/Program.fs
+++ src/WhereInTheWorld/Program.fs
@@ -1,6 +1,5 @@
 ﻿open System.IO
 open WhereInTheWorld.Update
-open WhereInTheWorld.Update.Utilities
 open Hopac
 
 let ensureDirectory () =
@@ -22,48 +21,27 @@ let main argv =
 
     let stopWatch = System.Diagnostics.Stopwatch.StartNew()
 
-    let updateProcess =
-        DataDownload.supportedCountries
-        |> Seq.map (fun sc ->
-            let code, _, _ = sc
-            (DataDownload.downloadPostalCodesForCountry
-            >=> DataImport.readPostalCodesFile) code
-        )
-        |> Job.conCollect
-        |> run
-        |> Seq.filter (function
-            | Error _ -> false
-            |_ -> true
-        )
-        |> Seq.collect (fun pcs ->
-            let (Ok postalCodes) = pcs
-            postalCodes
-        )
-        |> batchesOf 50_000
-        |> Seq.iter (fun b ->
-            b
-            |> UpdateProcess.updateAll
-            |> function
-            | Error e -> printfn "Didn't work because %A" e
-            | Ok o -> printfn "Succeeded"
-        )
+    DataDownload.supportedCountries
+    |> Seq.map (fun sc ->
+        let code, _, _ = sc
+        DataDownload.downloadPostalCodesForCountry code
+    )
+    |> Job.conCollect
+    |> run
+    |> ignore // save failed downloads
 
-    (* |> Seq.iter (fun batch ->
-        batch
-        |> Seq.map (fun sc ->
-            let code, _, _ = sc
-            UpdateProcess.updateCountry code
-        )
-        |> Job.conCollect
+    DataDownload.supportedCountries
+    |> Seq.map (fun sc ->
+        let code, _, _ = sc
+        UpdateProcess.updateCountry code
         |> run
-        |> Seq.iter (fun cd ->
-            match cd with
-            | Error error ->
-                printfn "Country download for %s failed with message: %A" "" error
-            | Ok countryCode -> printfn "Country download for %s succeeded" countryCode
-        )
-    ) *)
-
+    )
+    |> Seq.iter (fun cd ->
+        match cd with
+        | Error error ->
+            printfn "Country download for %s failed with message: %A" "" error
+        | Ok countryCode -> printfn "Country download for %s succeeded" countryCode
+    )
 
     stopWatch.Stop()
     printfn "Total time took %fms" stopWatch.Elapsed.TotalMilliseconds
