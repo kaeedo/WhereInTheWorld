5e09897077c2e038bf03f23ed33e775fcc485a89
Ito, Kai <Kai.Ito@zuehlke.com>
2018-07-11 17:00:04 +0200

    Start implementing actor

--- src/WhereInTheWorld/StatusPrinter.fs
+++ src/WhereInTheWorld/StatusPrinter.fs
@@ -2,9 +2,36 @@ namespace WhereInTheWorld
 
 open WhereInTheWorld.Utilities.Models
 open Hopac
+open Hopac.Infixes
 
 module StatusPrinter =
+    type Message =
+    | Started of string
+    | Tick
+    | Finished of string
 
+    type CounterActor =
+    | CA of Ch<Message>
+
+    let create : Job<CounterActor> =
+        job {
+            let inChannel = Ch ()
+            let state = ref (false, "|")
+            do! Job.foreverServer
+                 (inChannel >>= function
+                   | Started cc ->
+                        state := true, "-"
+                        Job.unit ()
+                   | Tick ->
+                        let symbol = if snd !state = "|" then "-" else "|"
+                        state := true, symbol
+                        Job.unit ()
+                   | Finished cc ->
+                        state := false, ""
+                        Job.unit ())
+            return CA inChannel
+        }
+// https://vasily-kirichenko.github.io/fsharpblog/actors
     let downloadStatusPrinter channel =
         job {
             let! status = Ch.take channel
