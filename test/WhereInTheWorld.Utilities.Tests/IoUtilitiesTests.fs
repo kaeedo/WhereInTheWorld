77d05ceed0437eb64466adf9e669b49e546f89fc
Ito, Kai <Kai.Ito@zuehlke.com>
2018-07-31 09:17:25 +0200

    Add tests project

---
+++ test/WhereInTheWorld.Utilities.Tests/IoUtilitiesTests.fs
@@ -0,0 +1,40 @@
+﻿namespace WhereInTheWorld.Utilities.Tests
+
+open System.IO
+open Xunit
+open Swensen.Unquote
+open Hedgehog
+open WhereInTheWorld.Utilities.IoUtilities
+
+module IoUtilitiesTests =
+    [<Fact>]
+    let ``Path combine infix should have same total length`` () =
+        property {
+            let! (firstPath, secondPath) = 
+                Gen.char 'a' 'z'
+                |> (Range.constant 1 9
+                    |> Gen.string) 
+                |> Gen.tuple
+
+            let actual = firstPath @@ secondPath
+            let totalLength = (firstPath + secondPath + Path.PathSeparator.ToString()).Length
+
+            test <@ actual.Length = totalLength @>
+        }
+        |> Property.check
+
+    [<Fact>]
+    let ``Path combine infix should contain each part`` () =
+        property {
+            let! (firstPath, secondPath) = 
+                Gen.char 'a' 'z'
+                |> (Range.constant 0 9
+                    |> Gen.string) 
+                |> Gen.tuple
+
+            let actual = firstPath @@ secondPath
+
+            test <@ actual.Contains(firstPath) @>
+            test <@ actual.Contains(secondPath) @>
+        }
+        |> Property.check
