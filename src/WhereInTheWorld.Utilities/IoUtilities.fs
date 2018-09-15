a3ea4fccadf9a9a2aba4c56b97c76d801cc5102c
Kai Ito <kai.ito@gmail.com>
2018-09-15 17:05:21 +0200

    Move supported countries to separate file

--- src/WhereInTheWorld.Utilities/IoUtilities.fs
+++ src/WhereInTheWorld.Utilities/IoUtilities.fs
@@ -13,3 +13,12 @@ module IoUtilities =
         let resourceStream = assembly.GetManifestResourceStream(resourceName)
         use reader = new StreamReader(resourceStream, Encoding.UTF8)
         reader.ReadToEnd()
+
+    let parseTsv (contents: string) =
+        contents
+        |> fun t -> t.Split([|Environment.NewLine|], StringSplitOptions.None)
+        |> Seq.filter (fun pc -> not (String.IsNullOrWhiteSpace(pc)))
+        |> Seq.map (fun pc ->
+            let line = pc.Split('\t')
+            line.[0], line.[1]
+        )
